3ee5b392858034ea3a07f649a1451e1fc4534515
TRI <rafael.tagirzyanov@yandex.ru>
2022-09-05 19:34:54 +0300

    Третья задача

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+ {
+    array [i] = new Random().Next(0, 50);
+    Console.Write(" " + num (i) + " ");
+ }
+Console.Write("]");
+
+int num (int a)
+{
+    return array[a];
+}
